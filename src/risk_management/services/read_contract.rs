1213a9833055c92e37f850848b44d93fb4238b74
Tom Dymel <td@wps.de>
2021-06-07 18:34:32 +0200

    test read contract

--- src/risk_management/services/read_contract.rs
+++ src/risk_management/services/read_contract.rs
@@ -11,3 +11,21 @@ impl ReadContract for RiskManagement {
         self.contracts.get(&number)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::risk_management::RiskManagement;
+    use crate::risk_management::services::{ApplicationInbox, ReadContract};
+    use crate::shared::domain_values::ContractNumber;
+
+    #[test]
+    fn read_contract() {
+        let risk_management = RiskManagement::new();
+        let number = ContractNumber::new("1234-1234-1234-1234".to_owned()).unwrap();
+        risk_management.confirm_signed_contract(number.clone());
+        let contract = risk_management.read_contract(number.clone());
+        assert!(contract.is_some());
+        let contract = contract.unwrap();
+        assert_eq!(contract.number(), &number);
+    }
+}
