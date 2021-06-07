57a3de9cadd050ac67fe2ac85caa253ded9344e0
Tom Dymel <td@wps.de>
2021-06-07 18:11:14 +0200

    Test sign contract

--- src/sales/services/sign_contract.rs
+++ src/sales/services/sign_contract.rs
@@ -16,3 +16,37 @@ impl SignContract for Sales {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
+
+    use crate::risk_management::RiskManagement;
+    use crate::sales::domain_values::{Amount, Brand, Car, Currency, Customer};
+    use crate::sales::Sales;
+    use crate::sales::services::{FillOutContract, SignContract};
+    use crate::shared::domain_values::{ContractNumber, SignDate};
+
+    #[test]
+    fn sign_contract() {
+        let sales = Sales::new();
+        let risk_management = RiskManagement::new();
+        let number = ContractNumber::new("1234-1234-1234-1234".to_owned()).unwrap();
+        let customer = Customer::new("Max".to_owned(), "Mustermann".to_owned());
+        let car = Car::new(Brand::Porsche);
+        let price = Amount::new(123423, Currency::Euro);
+        let sign_date = SignDate::from_date_time(DateTime::from_utc(NaiveDateTime::from_timestamp(32432423, 0), Utc));
+
+        sales.fill_out_contract(number.clone(), customer.clone(), car.clone(), price.clone());
+        assert!(!sales.contracts.all().is_empty());
+        let contract = sales.contracts.get(&number).unwrap();
+        assert!(!contract.is_signed());
+
+        assert!(risk_management.contracts.all().is_empty());
+        let signature = sales.sign_contract(&risk_management, number.clone(), sign_date);
+        assert!(signature.is_ok());
+        assert!(!risk_management.contracts.all().is_empty());
+        let contract = sales.contracts.get(&number).unwrap();
+        assert!(contract.is_signed());
+    }
+}
