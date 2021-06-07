9a564463d13a1c21fd2db4346d667cb2d1c55028
Tom Dymel <td@wps.de>
2021-06-07 18:18:02 +0200

    Test confirm_signed_contract

--- src/risk_management/services/application_inbox.rs
+++ src/risk_management/services/application_inbox.rs
@@ -11,3 +11,26 @@ impl ApplicationInbox for RiskManagement {
         self.contracts.save(number.clone(), Contract::new(number))
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::risk_management::RiskManagement;
+    use crate::shared::domain_values::ContractNumber;
+    use crate::risk_management::services::ApplicationInbox;
+
+    #[test]
+    fn confirm_signed_contract() {
+        let risk_management = RiskManagement::new();
+        let number = ContractNumber::new("1234-1234-1234-1234".to_owned()).unwrap();
+
+        assert!(risk_management.contracts.all().is_empty());
+        risk_management.confirm_signed_contract(number.clone());
+        assert!(!risk_management.contracts.all().is_empty());
+        let contract = risk_management.contracts.get(&number);
+        assert!(contract.is_some());
+        let contract = contract.unwrap();
+        assert_eq!(contract.number(), &number);
+        assert!(!contract.is_rated());
+        assert!(!contract.is_voted());
+    }
+}
