b645bb99338dcab690cdfa724bc7f519e0de3441
Tom Dymel <td@wps.de>
2021-06-07 18:33:13 +0200

    test list all contracts

--- src/risk_management/services/list_contracts.rs
+++ src/risk_management/services/list_contracts.rs
@@ -10,3 +10,21 @@ impl ListContracts for RiskManagement {
         self.contracts.all()
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::risk_management::RiskManagement;
+    use crate::shared::domain_values::ContractNumber;
+    use crate::risk_management::services::{ApplicationInbox, ListContracts};
+
+    #[test]
+    fn list_all_contracts() {
+        let risk_management = RiskManagement::new();
+        let number = ContractNumber::new("1234-1234-1234-1234".to_owned()).unwrap();
+        risk_management.confirm_signed_contract(number.clone());
+        assert_eq!(risk_management.list_all_contracts().len(), 1);
+        let contracts = risk_management.list_all_contracts();
+        let contract = contracts.get(0).unwrap();
+        assert_eq!(contract.number(), &number);
+    }
+}
